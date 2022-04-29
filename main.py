c41f44fee653b817a759d64d60ad417906a264c1
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-04-29 13:05:11 +1100

    Stage 1

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
 from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-from lxml import etree
-from string import punctuation
 from collections import Counter
+from lxml import etree
 
 
 xml_file = "news.xml"
-
 root = etree.parse(xml_file).getroot()
 news = root[0]
 
 heads = [el.text for n in news for el in n if el.get('name') == 'head']
-texts = (el.text for n in news for el in n if el.get('name') == 'text')
+texts = [el.text for n in news for el in n if el.get('name') == 'text']
 
-filter_items = list(punctuation) + stopwords.words('english')
-lemmatiser = WordNetLemmatizer()
-
-for head in heads:
-    text = next(texts)
-    tokens = word_tokenize(text.lower())
-    tokens_lemma = [lemmatiser.lemmatize(w) for w in tokens]
-    tokens_lemma_filtered = list(filter(lambda w: w not in filter_items, tokens_lemma))
-    tokens_lemma_filtered.sort(reverse=True)
-
-    counter = Counter(tokens_lemma_filtered)
-    most_common = [w for w, _ in counter.most_common(5)]
-
+for head, text in zip(heads, texts):
+    tokens = sorted(word_tokenize(text.lower()), reverse=True)
+    most_common = [w for w, _ in Counter(tokens).most_common(5)]
     print(''.join((head, ':')))
     print(' '.join(most_common))
