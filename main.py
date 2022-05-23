d6da31b3004cfe8bc5b76cd96a719ffa49da0e98
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-05-23 17:52:11 +1100

    Stage 3

--- main.py
+++ main.py
@@ -4,27 +4,32 @@ from nltk.corpus import stopwords
 from lxml import etree
 from string import punctuation
 from collections import Counter
+import nltk
+
 
 xml_file = "news.xml"
 
-root = etree.parse(xml_file).getroot()
-news = root[0]
+news_list = (news for news in etree.parse(xml_file).getroot()[0])
 
-heads = [el.text for n in news for el in n if el.get('name') == 'head']
-texts = (el.text for n in news for el in n if el.get('name') == 'text')
+filter_items = set(punctuation) | set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer().lemmatize
 
-filter_items = list(punctuation) + stopwords.words('english')
-lemmatiser = WordNetLemmatizer()
-
-for head in heads:
-    text = next(texts)
-    tokens = word_tokenize(text.lower())
-    tokens_lemma = [lemmatiser.lemmatize(w) for w in tokens]
-    tokens_lemma_filtered = list(filter(lambda w: w not in filter_items, tokens_lemma))
-    tokens_lemma_filtered.sort(reverse=True)
-
-    counter = Counter(tokens_lemma_filtered)
-    most_common = [w for w, _ in counter.most_common(5)]
-
-    print(''.join((head, ':')))
-    print(' '.join(most_common))
+n = 0
+while n < 10:
+    news = next(news_list)
+    head = news.find("value[@name='head']")
+    text = news.find("value[@name='text']")
+    tokens = (ntoken for token in word_tokenize(text.text.lower())
+              if (ntoken := lemmatizer(token)) not in filter_items)
+    most_common = []
+    counter = Counter(sorted(tokens, reverse=True)).most_common()
+    i = 0
+    for w, _ in counter:
+        if i == 5:
+            break
+        if nltk.pos_tag([w])[0][1] == 'NN':
+            most_common.append(w)
+            i += 1
+    n += 1
+    print(''.join((head.text, ':')))
+    print(*most_common, end='\n\n')
